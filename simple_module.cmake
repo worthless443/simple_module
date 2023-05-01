5b958923dea3c3ad1d9310ad5f6ae38d16c9736a
worthless443 <aissypewdiepie@gmail.com>
2023-05-02 02:06:55 +0530

    std headers as modules support with GCC

--- simple_module.cmake
+++ simple_module.cmake
@@ -1,3 +1,6 @@
+set(GENERIC_CXX_MODULE_FLAGS -std=c++20 -fmodules-ts -xc++-system-header)
+set(GCM_CACHE gcm.cache/usr/include/c++/12.2.1)
+
 function(check_no_import_stdlib result) 
 	set(stdlib_list iostream string vector) # add the entire list of standard headers?
 	cmake_parse_arguments(MOD "" "IF" "Else" ${ARGN})
@@ -17,6 +20,7 @@ function(check_no_import_stdlib result)
 			
 	endforeach()
 
+
 	set(${result} FALSE PARENT_SCOPE)
 	foreach(module ${modules})
 		string(REGEX MATCH "\<.*\>" match ${module})
@@ -27,7 +31,36 @@ function(check_no_import_stdlib result)
 			set(${result} TRUE PARENT_SCOPE)
 		endif()
 	endforeach()
+	set(std_modules ${modules} PARENT_SCOPE)
 endfunction()
+
+function(add_stdmodular_executable name)
+	cmake_parse_arguments(MOD "" "IF" "Else" ${ARGN})
+	set(source ${MOD_UNPARSED_ARGUMENTS})
+	check_no_import_stdlib(has_stdimport ${source})
+	if(CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+		set(CMAKE_CXX_EXTENSIONS OFF PARENT_SCOPE)
+		list(LENGTH std_modules len_std_modules)	
+		if(len_std_modules GREATER 0)
+			foreach(std_module ${std_modules})
+				string(REGEX MATCH "\<.*\>" match ${std_module})
+				if(NOT match STREQUAL "")
+					string(REGEX MATCH "[a-z]+" module ${std_module})
+					add_custom_target(${module} ALL)
+					add_custom_command(
+						TARGET ${module}
+						COMMAND ${CMAKE_CXX_COMPILER} ${GENERIC_CXX_MODULE_FLAGS} ${module}
+					)
+				endif()
+			endforeach()
+		endif()
+	elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+		message(FATAL_ERROR "std modules with Clang is not supported yet")
+	endif()
+	add_executable(${name} ${source})
+	target_compile_options(${name} PUBLIC -std=c++20 -fmodules-ts)
+endfunction()
+
 
 function(add_module_library name)
 	cmake_parse_arguments(MOD "" "IF" "Else" ${ARGN})
@@ -39,7 +72,7 @@ function(add_module_library name)
 			message(FATAL_ERROR "idk how to suppport standard headers with Clang yet, please use GCC")
 		endif()
 	endif()
-		
+			
 	add_library(${name})
 	set_target_properties(${name} PROPERTIES LINKER_LANGUAGE CXX)
 	target_compile_features(${name} PUBLIC cxx_std_20)
